b0bca970dd4b33376fb25169e439a6fd675b5545
Iago R <iago-r@outlook.com>
2025-04-10 10:53:57 -0300

    feat: enhancements to support result_id
    * updated findings_list_snippet.html to include result_id in the vote dropdown attribute (and in the javascript)
    
    * enhanced polls_filters.py to extract result_id from descriptions using regex
    
    * adjusted polls_plugin/models.py to reflect new database changes for result_id
    
    * adjusted the dump_training_sets.py to add the result_id as a field
    
    * updated polls_plugin/views.py to handle result_id in the vote-saving

--- polls_plugin/templatetags/polls_filters.py
+++ polls_plugin/templatetags/polls_filters.py
@@ -1,3 +1,5 @@
+import re
+
 from django import template
 
 register = template.Library()
@@ -6,3 +8,13 @@ register = template.Library()
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(str(key), None)
+
+
+@register.filter
+def extract_result_id(description):
+    pattern = r"\*\*?ResultId\*\*?: ([a-f0-9\-]+)"
+    match = re.search(pattern, description)
+
+    if match:
+        return match.group(1)
+    return None
