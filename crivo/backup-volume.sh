3d8b3f86c47a4a78dc362f6a95fba790ac3a95b7
Italo Cunha <cunha@dcc.ufmg.br>
2025-04-21 12:40:24 -0300

    feat: add script to backup crivo metadata

---
+++ crivo/backup-volume.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -eu
+
+VOLUME=projectname_defectdojo_crivo
+
+if ! docker volume ls -q | grep -w $VOLUME &>/dev/null; then
+    echo "docker volume $VOLUME not found, aborting"
+    exit 1
+fi
+
+DATE=$(date +%Y-%m-%d)
+BACKUP=$(pwd)/backup
+mkdir -p "$BACKUP"
+
+docker run --rm \
+  -v $VOLUME:/volume \
+  -v "$BACKUP:/backup" \
+  busybox \
+  tar --exclude=findings_polls.db -czf /backup/backup-$DATE-novotes.tar.gz \
+    -C /volume .
+
+docker run --rm \
+  -v $VOLUME:/volume \
+  -v "$BACKUP:/backup" \
+  busybox \
+  cp /volume/findings_polls.db /backup/findings_polls.db
